3bc6e0fc28580ce41f01c6fb1cde426cac971500
Alys Scorpio <alyssad1182@gmail.com>
2021-08-03 16:50:51 -0700

    creating home routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -74,3 +74,5 @@ router.post('/logout', (req,res) => {
         res.status(404).end();
     }
 });
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,33 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models')
+
+router.get('/login', async (req,res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/signup', async (req,res) => {
+    try {
+        res.render('signup');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 router.get('/', async (req,res) => {
     try {
-        res.render('homepage');
+        const postData = await Post.findAll({
+            order: [[ ' created_at', 'DESC']]
+        });
+
+        const blog_posts = postData.map((info) => info.get({ plain: true }));
+
+        res.render('home', {
+            blog_posts, logged_in: req.session.logged_in
+        });
     } catch (err) {
         res.status(500).json(err);
     }
