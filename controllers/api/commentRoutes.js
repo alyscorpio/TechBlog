36b0590e54fdc7c3747858afab99b886b72dfb74
Alys Scorpio <alyssad1182@gmail.com>
2021-08-03 16:30:52 -0700

    creating get and post routes for comments

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+// POST
+router.post('/', async (req,res) => {
+    try {
+        const commentData = await Comment.create ({...req.body, user_id:req.session.user_id});
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET
+router.get('/', async (req, res) => {
+    try {
+        const commentData = await Comment.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['id', 'username'],
+                }
+            ],
+            order: [['created_at', 'DESC']]
+        });
+
+        const comments = commentData.map((comment) =>
+        comment.get({ plain: true })
+        );
+    } catch (err) {
+        console.log(err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
+});
+
+module.exports = router;
